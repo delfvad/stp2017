5af4cc3266b323988d6f5848ec63c74dd8c1d39e
Vadim Zahariev <delfvad@gmail.com>
2017-10-13 21:43:14 +0300

    lab02 and lab03 has been added.

---
+++ lab02/example/02b-servlet-by-annotation/src/main/java/by/bsuir/MainServlet.java
@@ -0,0 +1,18 @@
+package by.bsuir;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/stp",
+            loadOnStartup = 1)
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+}
